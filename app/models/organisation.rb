cf2b093f7db7aae6559355281a748fc0c60cb1fc
steve-hun <samrithketisak.hun@gmail.com>
2025-05-18 12:32:11 +1000

    Update organisation model

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,3 +1,11 @@
 class Organisation < ApplicationRecord
-  has_many :users
+  
+  has_many :memberships
+  has_many :users, through: :memberships
+  has_many :posts
+
+  def active_users(organisation)
+    users.where(status: "active")
+  end
+  
 end
