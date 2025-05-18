4a082a38bc2fd48d1e9a16246624540a8cae9fab
steve-hun <samrithketisak.hun@gmail.com>
2025-05-18 14:39:03 +1000

    Fix syntax error in user model and remove name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,13 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
-  has_many: memberships
+  has_many :memberships
   has_many :organisations, through: :memberships
   has_many :posts
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
   # Validation when user try to update their profile
-  validates :name, presence: true
   validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, if: :password_digest_changed?
 
