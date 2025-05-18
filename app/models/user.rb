5885de16fd438cfda4d7d266fbb8da192b9ef4df
steve-hun <samrithketisak.hun@gmail.com>
2025-05-18 12:32:39 +1000

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+
+  has_many: memberships
+  has_many :organisations, through: :memberships
   has_many :posts
-  belongs_to :organisation, optional: true
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
@@ -10,4 +12,14 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, if: :password_digest_changed?
+
+  def membership_in(organisation)
+    memberships.find_by(organisation: organisation)
+  end
+
+  def active_in_org?(organisation)
+    membership = membership_in(organisation)
+    #TODO: Change hard-coded to enum
+    membership&.status == "active"
+  end
 end
