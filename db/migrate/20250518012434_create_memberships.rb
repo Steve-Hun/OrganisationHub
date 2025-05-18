6ce9da7228ebac1047339fe198cd39cf971b9ca0
steve-hun <samrithketisak.hun@gmail.com>
2025-05-18 12:14:47 +1000

    Create Memberships table

---
+++ db/migrate/20250518012434_create_memberships.rb
@@ -0,0 +1,14 @@
+class CreateMemberships < ActiveRecord::Migration[8.0]
+  def change
+    create_table :memberships do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :organisation, null: false, foreign_key: true
+      t.string :status
+
+      t.timestamps
+    end
+    
+    # Unique index to ensure a user joins an organisation once
+    add_index :memberships, [:user_id, :organisation_id], unique: true
+  end
+end
