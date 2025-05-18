79cf42bd7967afe4e2c9c2d6d619a221d0ed0e1d
steve-hun <samrithketisak.hun@gmail.com>
2025-05-18 14:39:24 +1000

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+org1 = Organisation.create!(name: "Coles")
+org2 = Organisation.create!(name: "Woolworths")
+org3 = Organisation.create!(name: "Aldi")
+
+
+user1 = User.create!(email_address: "john@example.com", password: "password")
+user2 = User.create!(email_address: "bob@example.com", password: "password")
+user3 = User.create!(email_address: "jane@example.com", password: "password")
+
+
+Membership.create!(user: user1, organisation: org1, status: "active")
+Membership.create!(user: user2, organisation: org1, status: "active")
+
+Membership.create!(user: user2, organisation: org2, status: "active")
