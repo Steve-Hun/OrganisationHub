b0da17723a73bbbad31cd8fcc2baa1c2a446c241
steve-hun <samrithketisak.hun@gmail.com>
2025-05-18 15:31:26 +1000

    Add function to retrieve active memberships of a user

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,8 @@
 class Membership < ApplicationRecord
   belongs_to :user
   belongs_to :organisation
+
+  def self.get_active_memberships(user)
+    Membership.where(user: user, status: "active")
+  end
 end
