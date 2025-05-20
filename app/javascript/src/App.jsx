ee7427495457113c1cfaa14566044b4137ac10d2
steve-hun <samrithketisak.hun@gmail.com>
2025-05-20 21:09:36 +1000

    Modify react app to include react routes

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,10 +1,22 @@
-export default function App() {
+import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import PostIndex from './pages/PostIndex';
+import PostShow from './pages/PostShow';
+import PostForm from './pages/PostForm';
+
+function App({ userId }) {
     return (
-        <div>
-            <h1>Hello World</h1>
-            <p>This is a React App</p>
+        <div className="react-app">
+            <Routes>
+                <Route path="/organisations/:organisationId/posts" element={<PostIndex userId={userId}  />} />
+                <Route path="/organisations/:organisationId/posts/new" element={<PostForm userId={userId} isNew={true} />} />
+                <Route path="/organisations/:organisationId/posts/:id" element={<PostShow userId={userId} />} />
+                <Route path="/organisations/:organisationId/posts/:id/edit" element={<PostForm userId={userId} isNew={false} />} />
+            </Routes>
         </div>
-    )
+    );
 }
+
+export default App;
 
 
