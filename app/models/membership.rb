bdc70e63478fce8517d9824ff5ab4f60b5780ea6
steve-hun <samrithketisak.hun@gmail.com>
2025-05-22 21:49:02 +1000

    Feat: add method for checking active membership

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -5,4 +5,8 @@ class Membership < ApplicationRecord
   def self.get_active_memberships(user)
     Membership.where(user: user, status: "active")
   end
+
+  def self.is_active_member?(user, organisation)
+    Membership.where(user: user, organisation: organisation, status: "active").exists?
+  end
 end
